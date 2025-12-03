4d991fc849af10892fe0342be16089bc161c2098
j-o-lopez <jl82838@utexas.edu>
2025-12-02 22:54:33 -0600

    commented out provider visualizations pages to avoid errors.

--- frontend/src/pages/ProviderVisualizations.jsx
+++ frontend/src/pages/ProviderVisualizations.jsx
@@ -1,15 +1,12 @@
-import ItemsByCategoryBar from "../components/charts/ItemsByCategoryBar";
-import RequestStatusPie from "../components/charts/RequestStatusPie";
-import RequestsOverTimeLine from "../components/charts/RequestsOverTimeLine";
+// import ItemsByCategoryBar from "../components/charts/ItemsByCategoryBar";
+// import RequestStatusPie from "../components/charts/RequestStatusPie";
+// import RequestsOverTimeLine from "../components/charts/RequestsOverTimeLine";
 
 const ProviderVisualizations = () => {
   return (
     <div style={{ padding: "2rem" }}>
       <h1>Project Pencil ATX Visualizations</h1>
 
-      <ItemsByCategoryBar />
-      <RequestStatusPie />
-      <RequestsOverTimeLine />
     </div>
   );
 };
