a314449e9a63037bc8621e25120f50b41c193a5a
mahikadawar <mahika.dawar@utexas.edu>
2025-12-02 18:30:00 -0600

    connect d3 graphs to visualizations page

--- frontend/src/pages/Visualizations.jsx
+++ frontend/src/pages/Visualizations.jsx
@@ -1,10 +1,17 @@
+import FoodbanksBarChart from "../components/charts/FoodbanksBarChart";
+import ProgramPieChart from "../components/charts/ProgramPieChart";
+import SponsorsLineChart from "../components/charts/SponsorsLineChart";
+
 const Visualizations = () => {
-	return (
-		<div>
-			<h1>My D3 Visualizations</h1>
-			{/* add D3 graphs here */}
-		</div>
-	);
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>FoodBankConnect Visualizations</h1>
+
+      <FoodbanksBarChart />
+      <ProgramPieChart />
+      <SponsorsLineChart />
+    </div>
+  );
 };
 
 export default Visualizations;
