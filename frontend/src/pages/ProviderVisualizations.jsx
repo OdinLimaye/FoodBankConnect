8d530df9dc6c367e97ab0f5b5be0fc1074d13531
mahikadawar <mahika.dawar@utexas.edu>
2025-12-02 18:18:20 -0600

    page for provider visualizations

---
+++ frontend/src/pages/ProviderVisualizations.jsx
@@ -0,0 +1,10 @@
+const ProviderVisualizations = () => {
+	return (
+		<div>
+			<h1>Provider D3 Visualizations</h1>
+			{/* add their site D3 graphs here */}
+		</div>
+	);
+};
+
+export default ProviderVisualizations;
